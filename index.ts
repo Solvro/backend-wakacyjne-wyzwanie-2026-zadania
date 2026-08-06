20ab19643a590f22de04c49445bb775d26825499
“Aleksandra <musijenkoaleksandra@gmail.comgit config --global user.name Aleksandra>
2026-08-06 20:03:56 +0200

    index.ts - updated missing instructions within a file

--- index.ts
+++ index.ts
@@ -2,14 +2,13 @@ import fs from 'fs';
 
 const filename = 'elo-zelo.txt'; // define filename
 const minutes = new Date().getMinutes();
-// const minutes = 0; // test when minutes is 0
+// const minutes = 0; // test when minutes equals 0
 const value = 'elo-zelo\n'; // define input value 
 
-function eloZelo(filename: string, minutes: number, value: string): void {
-   fs.writeFileSync(filename, '');
-   for (let i = 0; i < minutes; i++) 
-         fs.appendFileSync(filename, value);
+function eloZelo(filename: string, minutes: number, value: string): void { // use void to handle when minutes equals 0
+   fs.writeFileSync(filename, ''); // write empty string to the file
+   for (let i = 0; i < minutes; i++)  // iterate through current minutes value
+         fs.appendFileSync(filename, value); // input value i times 
 }
 
-
-eloZelo(filename, minutes, value);
+eloZelo(filename, minutes, value); // call out the function
