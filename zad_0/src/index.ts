5ad5a3be0a2d055c9313af5064e7d30816ff2148
KLTPL <105875079+KLTPL@users.noreply.github.com>
2026-08-07 11:05:50 +0200

    feat(zad_0): add error handling when writting to file

--- zad_0/src/index.ts
+++ zad_0/src/index.ts
@@ -4,7 +4,13 @@ function eloZelo(amount: number) {
   const value = "Elo żelo";
   const file = "elo-żelo.txt";
   const content = Array(amount).fill(value).join("\n");
-  fs.writeFileSync(file, content)
+  fs.writeFile(file, content, err => {
+    if (err) {
+      console.error("Error:", err);
+    } else {
+      console.log("Written to file successfully");
+    }
+  })
 }
 
 const minutes = (new Date).getMinutes();
