f7bfbf957d3360e66fe799d30a8ac7d92dd8acc5
marils25 <maria.ilska.2021@alo.pwr.edu.pl>
2026-08-07 17:21:06 +0200

    elo zelo

---
+++ index.ts
@@ -0,0 +1,13 @@
+const fs = require('node:fs');
+
+let text = "";
+
+function eloZelo(n: number): void{
+  for(let i=0; i<n; i++){
+    text += "Elo żelo\n"
+  }
+
+  fs.writeFileSync('elo-żelo.txt',text)
+}
+
+eloZelo(new Date().getMinutes());
