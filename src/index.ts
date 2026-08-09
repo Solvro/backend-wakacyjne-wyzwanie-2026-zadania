8ba5f4343e95772d858780d8f14e47fb8d34cb15
wwpwr2026 <ww.pwr2026@protonmail.com>
2026-08-09 04:33:49 +0200

    refactor: add better typing

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
 import fs from "node:fs";
 
-function eloZelo(repeatCount: number){
-    fs.writeFileSync("elo-zelo.txt", "Elo żelo\n".repeat(repeatCount));
+function assertType<T>(val: unknown): asserts val is T {}
+
+function eloZelo(repeatCount: number): void {
+    const fileName: string = "elo-zelo.txt";
+    const message: string = "Elo żelo\n";
+    const data: string = message.repeat(repeatCount);
+
+    try{
+        fs.writeFileSync(fileName, data);
+    } catch (err) {
+        assertType<Error | AggregateError>(err);  // https://nodejs.org/api/fs.html#fswritefilefile-data-options-callback
+        console.error(`Failed to write to file. ${err.name}: ${err.message}`);
+    }
 }
 
-eloZelo(new Date().getMinutes());
+const currentMinutes = new Date().getMinutes();
+eloZelo(currentMinutes);
