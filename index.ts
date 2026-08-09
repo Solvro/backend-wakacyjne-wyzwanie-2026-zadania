e06331136535548f53a73193bdf5fa65ed15d465
Czarek Laptop <czarek.zatylny@gmail.com>
2026-08-09 14:32:45 +0200

    Zastosowano poprawki: jednorazowy zapis do pliku, zamiast wielorazowego, oraz wykorzystanie try/catch przy writeFileSync

--- index.ts
+++ index.ts
@@ -1,15 +1,18 @@
 import * as fs from 'fs';
 
 const outputPath: string = './elo-żelo.txt';
-const content: string = "Elo żelo\n";
+let content: string = "";
 
 function eloZelo (n: number) : void {
-    fs.writeFileSync(outputPath, "");
     for (let i = 0; i < n; i++) {
-        fs.appendFileSync(outputPath, content, 'utf8');
+        content += "Elo żelo\n"
+    }
+    try {
+        fs.writeFileSync(outputPath, content, 'utf8');
+    } catch (error) {
+        console.error('Error writing file:', error);
     }
 }
-
 
 const minute: number = new Date().getMinutes();
 
