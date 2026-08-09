2cc702009294e4fd9d84b75884131fd1605ae7dd
Nadia Gill <nadia.gill2006@gmail.com>
2026-08-09 22:18:07 +0200

    refactor: code review fixes

--- index.ts
+++ index.ts
@@ -1,10 +1,13 @@
 import fs from 'fs';
 
-function eloZelo(repeat: number) {
-    const text = "Elo żelo\n".repeat(repeat);
+function eloZelo(repeat: number):void {
+    const text = "Elo żelo\n";
+    const repeated_text = text.repeat(repeat);
+    const file_name = 'elo-zelo';
     
-    try { fs.writeFileSync('./elo-żelo.txt', text); } 
+    try { fs.writeFileSync(`./${file_name}.txt`, repeated_text); } 
     catch(err) { console.error(err); }
 }
 
-eloZelo(new Date().getMinutes());
+const arg = new Date().getMinutes();
+eloZelo(arg);
