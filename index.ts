79a6299551c6a893f49f5fd65628fb2d560315d3
srbbxl <srbbxl@proton.me>
2026-08-10 19:06:42 +0200

    fix: poprawka - wszystko wedlug wytycznych

--- index.ts
+++ index.ts
@@ -1,14 +1,16 @@
 import * as fs from 'fs';
 
-const plik: string = "elo-żelo.txt";
-
 function eloZelo(ilosc_elozelo: number): void {
-    for (let i = 0; i < ilosc_elozelo; i++) {
-        const zawartosc: string = "Elo Żelo\n".repeat(ilosc_elozelo);
-        fs.writeFileSync(plik, zawartosc, "utf8");
-    }
+    const plik: string = "elo-żelo.txt";
+
+    const zawartosc: string = "Elo Żelo\n".repeat(ilosc_elozelo);
+    fs.writeFileSync(plik, zawartosc, "utf8");
 }
 
 let minuty: number = new Date().getMinutes();
 
-eloZelo(minuty);
+try {
+    eloZelo(minuty);
+} catch (e) {
+    console.error(e, "nie udalo sie prawidlowo utworzyc pliku");
+}
