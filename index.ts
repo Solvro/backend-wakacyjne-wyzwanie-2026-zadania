d12efcc962f107f474124be8cddb9beb9c401ca1
Emilia Morawska <emilia.morawska.06@gmail.com>
2026-08-10 18:16:35 +0200

    Fix: Applied feedback

--- index.ts
+++ index.ts
@@ -1,16 +1,15 @@
-import fs from 'node:fs';
+import fs from "node:fs";
 
-const baseText:string = 'Elo żelo\n';
+const baseText: string = "Elo żelo\n";
 
-function eloZelo(repeats:number):void{
-
-    const content:string = baseText.repeat(repeats);
-    fs.writeFile('elo-żelo.txt',content,err=>{
-        if(err){
-            console.error(err);
-        } })
+function eloZelo(repeats: number): void {
+  const content: string = baseText.repeat(repeats);
+  try {
+    fs.writeFileSync("elo-żelo.txt", content);
+  } catch (err) {
+    console.error(err);
+  }
 }
 
-
-const minutes:number = new Date().getMinutes();
+const minutes: number = new Date().getMinutes();
 eloZelo(minutes);
