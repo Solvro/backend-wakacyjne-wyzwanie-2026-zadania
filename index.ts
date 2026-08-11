4b4ce77641d59d30dca6b5af84206e28684c1792
w k <kukann@interia.pl>
2026-08-11 17:03:13 +0200

    Poprawka zad0

--- index.ts
+++ index.ts
@@ -3,11 +3,14 @@ const filePath: string = "elo-żelo.txt";
 const now: Date = new Date();
 const n=now.getMinutes();
 
-function eloZelo(n: number){
-    for(var i=1; i<=n; i++){
-        fs.appendFileSync(filePath, "Elo żelo\n", 'utf-8');
+function eloZelo(num: number){
+    const out_string = "Elo żelo\n".repeat(num);
+    try{
+        fs.writeFileSync(filePath, out_string, 'utf-8');
+        console.log("Wpisano Elo żelo", num, "razy\n");
+    }catch(e){
+        console.log(e);
     }
-    console.log("Wpisano Elo żelo", n, "razy\n");
 }
 
 eloZelo(n);
