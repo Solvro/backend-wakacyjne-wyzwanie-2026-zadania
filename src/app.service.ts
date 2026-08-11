9a7e770e00d623fae6ee767b8291ced1a661b17b
Robert Włodkowski <287445@student.pwr.edu.pl>
2026-08-11 23:56:31 +0200

    feat: zadanie-1

---
+++ src/app.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+
+export interface CatPics {
+  message : string,
+  funnyPictureURL : string
+}
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+  
+  getCoffee(): CatPics {
+    return {"message": "I'm a teapot", "funnyPictureURL": "https://http.cat/images/418.jpg"};
+  }
+}
