a61fd5bcf8773e8d7fcecf344cf0f70c22d903ef
Someones-account <sashko23012006.1@gmail.com>
2026-08-11 14:10:20 +0200

    Add error handling

--- index.ts
+++ index.ts
@@ -3,8 +3,13 @@ import * as fs from 'fs';
 function eloZelo(count: number): void {
   const textToSave: string = 'Elo żelo\n'.repeat(count);
   
-  fs.writeFileSync('elo-żelo.txt', textToSave, 'utf-8');
-  console.log(`Pomyślnie zapisano frazę "Elo żelo" ${count} razy w pliku elo-żelo.txt`);
+  try {
+    fs.writeFileSync('elo-żelo.txt', textToSave, 'utf-8');
+    console.log(`Pomyślnie zapisano frazę "Elo żelo" ${count} razy w pliku elo-żelo.txt`);
+  }
+  catch (err) {
+    console.error('Wystąpił błąd podczas zapisu do pliku:', err)
+  }
 }
 
 const currentMinutes: number = new Date().getMinutes();
