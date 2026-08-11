38be94153a7de5ffeac16e35699850f227f235f6
jakub206 <jakub_dluzak@wp.pl>
2026-08-11 13:20:10 +0200

    feat: zadanko 1

---
+++ src/app.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Header, HttpCode, Redirect } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller("/solvro")
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  @Redirect('https://www.solvro.pwr.edu.pl/', 307)
+  goToWebsite() {
+    return "Redirecting to Solvro website...";
+  }
+
+  @Get("brewCoffee")
+  @HttpCode(418)
+  @Header("Content-Type", "application/json")
+  brewCoffee() {
+    return this.appService.getBrewCoffee();
+  }
+}
