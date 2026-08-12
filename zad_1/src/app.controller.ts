0f47022a111b81f4e72ffef034fd7582b59ecd14
KLTPL <105875079+KLTPL@users.noreply.github.com>
2026-08-12 17:02:20 +0200

    feat: task 1 solution

--- zad_1/src/app.controller.ts
+++ zad_1/src/app.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+  Controller,
+  Get,
+  Header,
+  HttpCode,
+  HttpStatus,
+  Redirect,
+} from "@nestjs/common";
+import { AppService } from "./app.service";
 
-@Controller()
+@Controller("solvro")
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Redirect("https://solvro.pwr.edu.pl", HttpStatus.TEMPORARY_REDIRECT)
+  goToSolvro() {}
+
+  @Get("brewCoffee")
+  @HttpCode(HttpStatus.I_AM_A_TEAPOT)
+  @Header("Content-Type", "application/json")
+  getCoffee() {
+    return {
+      message: "I'm a teapot",
+      funnyPictureURL: "https://http.cat/images/418.jpg",
+    };
   }
 }
