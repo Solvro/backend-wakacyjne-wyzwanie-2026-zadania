fe793c8ffe944a26285b21b608ad0c4274b65437
marils25 <maria.ilska.2021@alo.pwr.edu.pl>
2026-08-12 19:37:42 +0200

    Zadanie 1 - Maria Ilska

---
+++ zadanie1/src/app.controller.ts
@@ -0,0 +1,26 @@
+import { Controller, Get, Header, Headers, HttpCode, Redirect } from '@nestjs/common';
+import { AppService } from './app.service';
+import { REDIRECT_METADATA } from '@nestjs/common/constants';
+
+
+@Controller('solvro')
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('brewCoffee')
+  @HttpCode(418)
+  @Header('Content-Type', 'application/json')
+  getBrewCoffee(): {"message": string, "funnyPictureURL": string} {
+    return {
+    "message":  "I'm a teapot",
+    "funnyPictureURL": "https://http.cat/images/418.jpg"
+    }
+  }
+
+  
+  @Get()
+  @Redirect('https://solvro.pwr.edu.pl', 307)
+  getSolvro(){
+
+  }
+}
