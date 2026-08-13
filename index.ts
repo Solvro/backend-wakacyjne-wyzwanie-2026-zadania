f6a5449c911474b212160e2d89f21058b0e91b1d
Jan Wojtysiak <jan.wojtysiak.mail@gmail.com>
2026-08-13 13:20:42 +0200

    zadanie 1

--- index.ts
+++
@@ -1,9 +0,0 @@
-import { writeFileSync } from 'fs';
-
-function eloZelo(a: number) {
-    writeFileSync('./elo-żelo.txt', 'Elo żelo\n'.repeat(a));
-}
-
-const d = new Date();
-let minutes = d.getMinutes();
-eloZelo(minutes);
