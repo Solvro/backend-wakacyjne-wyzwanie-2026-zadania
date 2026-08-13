4b4212ceda3c63b1576fb0d9d464918c272a3a43
Papit00_ <golinski-pawel@wp.pl>
2026-08-13 17:33:53 +0200

    feat: Add implementation for zad 1

---
+++ zad-1/src/app.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Header, HttpCode, Redirect } from '@nestjs/common';
+import { AppService, BrewCoffeeResponse, RedirectToSolvroResponse } from './app.service';
+
+@Controller('/solvro')
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  @Redirect("https://solvro.pwr.edu.pl", 307)
+  redirectToSolvro(): RedirectToSolvroResponse {
+    return this.appService.redirectToSolvro();
+  }
+
+  @Get('/brewCoffee')
+  @Header('Content-Type', "application/json")
+  @HttpCode(418)
+  brewCoffee(): BrewCoffeeResponse {
+    return this.appService.brewCoffee();
+  }
+}
