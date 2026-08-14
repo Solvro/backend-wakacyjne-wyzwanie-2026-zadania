249ddc800b53860c22cd1b9518abe8d17715538a
Volodymyr1233 <105359387+Volodymyr1233@users.noreply.github.com>
2026-08-14 14:02:18 +0200

    feat: task 1 done

---
+++ task1/src/app.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Redirect, HttpCode, Header } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller('solvro')
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  @Redirect('https://solvro.pwr.edu.pl', 307)
+  redirectToSolvroWebSite() {}
+
+  @Get('brewCoffee')
+  @HttpCode(418)
+  @Header('Content-Type', 'application/json')
+  brewCoffee() {
+    return this.appService.getCoffeeResponse();
+  }
+}
