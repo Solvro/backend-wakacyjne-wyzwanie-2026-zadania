4189c5aeae412056b6396e9b7dea5f33404e83c6
Kostya-Iva <kostyamega1810@gmail.com>
2026-08-21 21:35:21 +0200

    created prisma database

---
+++ zadanie2/src/app.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Header, HttpCode } from '@nestjs/common';
+import { AppService } from './app.service';
+import { Redirect } from '@nestjs/common';
+
+@Controller('solvro')
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+  
+  @Get()
+  @Redirect('https://solvro.pwr.edu.pl', 307)
+  redirectToSolvro() {}
+
+  @Get('brewCoffee')
+  @HttpCode(418)
+  @Header('Content-Type', 'application/json')
+  brewCoffee() {
+    return this.appService.brewCoffee();
+  }
+
+}
