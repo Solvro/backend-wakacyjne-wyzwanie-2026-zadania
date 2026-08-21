d79e8fee895c5876cf122d54c97d691fc79a345a
Yevhenii Duranin <284337@student.pwr.edu.pl>
2026-08-21 19:54:14 +0200

    Fixed stylization and separated greeting

--- index.ts
+++ index.ts
@@ -1,18 +1,20 @@
 const fs = require('node:fs');
 
-function eloZelo(number: number): void {
+const greet = "Elo żelo\n";
+
+function eloZelo(num: number): void {
   let text = "";
 
-  for (let i = 0; i < number; i++){
-    text = text + "Elo żelo\n";
+  for (let i = 0; i < num; i++) {
+    text += greet;
   }
 
 
   try {
     fs.writeFileSync("elo-żelo.txt", text, "utf-8");
   } catch (err) {
-  console.error(err);
-}
+    console.error(err);
+  }
 }
 
 const nowMinutes = new Date().getMinutes();
