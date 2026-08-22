06504ee67a033217f74e36d9a0d608daab625335
Rafzzz3 <zagularafal@gmail.com>
2026-08-22 16:39:32 +0200

    zadanie2

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TripController } from './trip/trip.controller';
-
+import { PrismaService } from './trip/prisma/prisma.service';
 @Module({
   imports: [],
   controllers: [AppController, TripController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
