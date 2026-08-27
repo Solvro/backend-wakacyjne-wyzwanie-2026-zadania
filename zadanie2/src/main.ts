129f7d798d4aaa7f61cf637bd9d18110bc68af65
sdmati <msiudy.ms@gmail.com>
2026-08-27 20:05:39 +0200

    feat: konfiguracja NestJS, Swagger, validationpipe

---
+++ zadanie2/src/main.ts
@@ -0,0 +1,30 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Trip Expenses API')
+    .setDescription('Dokumentacja API do zarządzania wycieczkami, uczestnikami i wydatkami')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}/api`);
+}
+bootstrap();
