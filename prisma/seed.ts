c04c712267f617c9d021f8bfa3a0feeb19a25500
Regulewicz <kur33g@gmail.com>
2026-08-29 00:03:58 +0200

    poprawione

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,10 +1,13 @@
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient, TripStatus, ExpenseCategory } from '../generated/prisma'
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
+const adapter = new PrismaPg({ 
+  connectionString: process.env.DATABASE_URL as string 
+})
 const prisma = new PrismaClient({ adapter })
 
 async function main() {
+
   const michal = await prisma.participant.create({
     data: {
       name: 'Michal',
@@ -14,10 +17,10 @@ async function main() {
     },
   })
 
+
   const trip = await prisma.trip.create({
     data: {
       title: 'Morze',
-      participant: michal.ParticipantID, 
       place: 'Jastarnia',
       start: new Date('2026-07-01'),
       end: new Date('2026-07-10'),
@@ -26,17 +29,25 @@ async function main() {
     },
   })
 
+  await prisma.tripParticipant.create({
+    data: {
+      tripId: trip.id,
+      participantId: michal.id,
+    },
+  })
+
+ 
   await prisma.expense.create({
     data: {
-      note: 'Bilety lotnicze',
-      payerID: michal.ParticipantID,
-      tripID: trip.TripID,
+      note: 'Meleks',
+      payerId: michal.id,
+      tripId: trip.id,
       amount: 800.0,
       category: ExpenseCategory.TICKETS,
     },
   })
 
-  console.log('Seed zakończony pomyślnie!')
+
 }
 
 main()
