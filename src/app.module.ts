7c686bf01681b8f19e1497143f1be8e63d0acf17
Kostya-Iva <kostyamega1810@gmail.com>
2026-08-28 20:36:38 +0200

    zadanie 3

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ExpensesModule } from './expenses/expenses.module';
+import { ParticipantsModule } from './participants/participants.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { TripsModule } from './trips/trips.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, TripsModule, ParticipantsModule, ExpensesModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/participants/participants.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateParticipantDto } from './dto/create-participant.dto';
+import { UpdateParticipantDto } from './dto/update-participant.dto';
+
+@Injectable()
+export class ParticipantsService {
+  constructor(private prisma: PrismaService) {}
+
+  create(createParticipantDto: CreateParticipantDto) {
+    return this.prisma.participant.create({ data: createParticipantDto });
+  }
+
+  findAll() {
+    return this.prisma.participant.findMany();
+  }
+
+  findOne(id: number) {
+    return this.prisma.participant.findUnique({ where: { id } });
+  }
+
+  update(id: number, updateParticipantDto: UpdateParticipantDto) {
+    return this.prisma.participant.update({ where: { id }, data: updateParticipantDto });
+  }
+
+  remove(id: number) {
+    return this.prisma.participant.delete({ where: { id } });
+  }
+}
