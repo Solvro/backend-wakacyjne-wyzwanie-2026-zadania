932b9c8f78511aac31517ef5509c5e5f1a5d2ba6
Stanisław Synowiec <stasyn1410@gmail.com>
2026-08-29 19:48:30 +0200

    feat: add trip & expense CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ParticipantsModule } from './participants/participants.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ExpenseModule } from './expense/expense.module';
+import { TripModule } from './trip/trip.module';
 
 @Module({
-  imports: [PrismaModule, ParticipantsModule],
+  imports: [PrismaModule, ParticipantsModule, ExpenseModule, TripModule],
   controllers: [AppController],
   providers: [AppService],
 })
