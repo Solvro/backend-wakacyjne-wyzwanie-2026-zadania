93516763f9ed17e434b826c3c40e8b2608651854
Mikołaj Rudnicki <mikolaj@rudnicki.dev>
2026-08-30 16:24:45 +0200

    feat: crud implementation with swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
+import { ParticipantModule } from './participant/participant.module';
+import { TripModule } from './trip/trip.module';
+import { ExpenseModule } from './expense/expense.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, ParticipantModule, TripModule, ExpenseModule],
   controllers: [AppController],
   providers: [AppService],
 })
