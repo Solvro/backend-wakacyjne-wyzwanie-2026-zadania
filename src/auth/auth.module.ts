3cc658030df49df337b6babdbf054b9b820a5c3f
Cezary Zatylny <czarek.zatylny@gmail.com>
2026-09-04 23:56:09 +0200

    Dodanie endpointu z logowaniem, który zwraca token JWT

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { DatabaseModule } from '../database/database.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    DatabaseModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+    })
+  ],
   providers: [AuthService],
   controllers: [AuthController]
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,16 @@
 import { ConflictException, Injectable } from '@nestjs/common';
 import {DatabaseService} from "../database/database.service";
 import {RegisterDto} from "./dto/register.dto";
+import {LoginDto} from "./dto/login.dto";
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly databaseService: DatabaseService) {}
+    constructor(
+        private readonly databaseService: DatabaseService,
+        private readonly jwtService: JwtService
+    ) {}
 
     async register(registerDto: RegisterDto) {
         const existingUser = await this.databaseService.user.findUnique({
@@ -28,6 +33,32 @@ export class AuthService {
         return {
             id: newUser.id,
             email: newUser.email,
+        };
+    }
+
+    async login(loginDto: LoginDto) {
+        const user = await this.databaseService.user.findUnique({
+            where: {email: loginDto.email},
+        });
+
+        if (!user) {
+            throw new ConflictException('Niepoprawny email lub hasło');
+        }
+
+        const isPasswordValid = await bcrypt.compare(loginDto.password, user.password);
+        if (!isPasswordValid) {
+            throw new ConflictException('Niepoprawny email lub hasło');
+        }
+
+        const payload = {
+            sub: user.id,
+            email: user.email,
+            timestamp: Date.now()
+        };
+        const token = this.jwtService.sign(payload);
+
+        return {
+            accessToken: token,
         };
     }
     }
