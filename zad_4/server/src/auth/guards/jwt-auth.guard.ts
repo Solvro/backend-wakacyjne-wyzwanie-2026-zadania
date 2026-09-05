67b02e843911231896b9d4540c9e7a9f1af90fa2
KLTPL <105875079+KLTPL@users.noreply.github.com>
2026-09-05 22:29:54 +0200

    feat: add @Public decorator to make endpoints public event when using the JwtAuthGuard

--- zad_4/server/src/auth/guards/jwt-auth.guard.ts
+++ zad_4/server/src/auth/guards/jwt-auth.guard.ts
@@ -1,8 +1,33 @@
-import { Injectable, UnauthorizedException } from "@nestjs/common";
+import {
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
 import { AuthGuard } from "@nestjs/passport";
+import { IS_PUBLIC_KEY } from "../decorators/public.decorator";
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard("jwt") {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
+  // activates first to check
+  canActivate(context: ExecutionContext) {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    // if there is a @Public decorator skip checking
+    if (isPublic) {
+      return true;
+    }
+
+    return super.canActivate(context);
+  }
+  // activates after canActivate
   handleRequest<TUser>(err: Error, user: TUser, info: unknown): TUser {
     if (info instanceof Error && info.name === "TokenExpiredError") {
       throw new UnauthorizedException("Token expired");
