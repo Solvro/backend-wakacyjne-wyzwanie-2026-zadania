8f5c7dab3473d8cc8d05edcf1d430b55e49e2476
KLTPL <105875079+KLTPL@users.noreply.github.com>
2026-09-05 18:12:50 +0200

    feat: add error handling when token expires

--- zad_4/server/src/auth/guards/jwt-auth.guard.ts
+++ zad_4/server/src/auth/guards/jwt-auth.guard.ts
@@ -1,5 +1,17 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard("jwt") {}
+export class JwtAuthGuard extends AuthGuard("jwt") {
+  handleRequest<TUser>(err: Error, user: TUser, info: unknown): TUser {
+    if (info instanceof Error && info.name === "TokenExpiredError") {
+      throw new UnauthorizedException("Token expired");
+    }
+
+    if (err || !user) {
+      throw err || new UnauthorizedException("Access forbiden or bad token");
+    }
+
+    return user;
+  }
+}
