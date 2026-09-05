c964828c3831ddf97e4a2f6e3195d6a3f0567cc0
KLTPL <105875079+KLTPL@users.noreply.github.com>
2026-09-05 21:27:57 +0200

    feat: add auth roles checks (guards and decorator)

---
+++ zad_4/server/src/auth/guards/roles.guard.ts
@@ -0,0 +1,42 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+} from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { UserRole } from "generated/prisma/client";
+import { ROLES_KEY } from "../decorators/roles.decorator";
+
+interface RequestWithUser extends Request {
+  user: {
+    id: number;
+    email: string;
+    role: UserRole;
+  };
+}
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<UserRole[]>( // reflector snacs metadata
+      ROLES_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+
+    if (!requiredRoles) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest<RequestWithUser>();
+    const user = request.user;
+
+    if (!requiredRoles.includes(user.role)) {
+      throw new ForbiddenException("Role required");
+    }
+
+    return true;
+  }
+}
