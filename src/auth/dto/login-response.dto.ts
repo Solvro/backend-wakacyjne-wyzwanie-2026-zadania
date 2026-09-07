74fb4f3e1086cad02d17988dda0096d4be05975a
Stanisław Synowiec <stasyn1410@gmail.com>
2026-09-07 18:30:12 +0200

    feat: add login DTOs for user authentication

---
+++ src/auth/dto/login-response.dto.ts
@@ -0,0 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class LoginResponseDto {
+  @ApiProperty({
+    example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...',
+    description: 'Token JWT (access_token)',
+  })
+  access_token: string;
+
+  @ApiProperty({
+    example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...',
+    description: 'Token JWT (alias token)',
+  })
+  token: string;
+}
